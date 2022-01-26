54e3ed299426ce5513b40ad30570369bb89bac94
Rohit Kumar <r0hitk@Rohits-Mac.local>
2022-01-27 03:44:33 +0530

    refactor and base structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Box, InputBase } from "@material-ui/core";
 import { Autocomplete } from "@react-google-maps/api";
-import SearchIcon from '@material-ui/icons';
+import SearchIcon from "@material-ui/icons/Search";
+
+import useStyles from "./styles";
 
 const Header = () => {
-    return (
-        <AppBar position="static">
-            <Toolbar className={classes.toolbar}>
-                <Typography variant="h5" className={classes.title}>
-                    Travel Advisor
-                </Typography>
-                <Box display="flex">
-                    <Typography variant="h6">
-                        Explore new places
-                    </Typography>
-                    <Autocomplete>
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
-                        </div>
-                    </Autocomplete>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
-}
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6">Explore new places</Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import useStyles from "./styles";
+import GoogleMapReact from "google-map-react";
+import { Typography, Paper, useMediaQuery } from "@material-ui/core";
+import Rating from "@material-ui/lab";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 
 const Map = () => {
-    return (
-        <div>
-            <h1>
-                Map
-            </h1>
-        </div>
-    );
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyA2yRxUJKs1W_CRhsS-VrUNwmwnvqNbNNw" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
 export default Map;
